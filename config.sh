6f9ee6f5a1ed7bd83b9882123cfdce9cb5ea4630
Ivan Shvedunov <ivan4th@gmail.com>
2016-11-03 10:05:49 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,9 +1,3 @@
-# Optional: Deploy cluster web interface.
-ENABLE_CLUSTER_UI="${ENABLE_CLUSTER_UI:-true}"
-
-# Timeout (in seconds) to wait for each addon to come up
-DOCKER_IN_DOCKER_ADDON_TIMEOUT="${DOCKER_IN_DOCKER_ADDON_TIMEOUT:-180}"
-
 # Apiserver port
 APISERVER_PORT=${APISERVER_PORT:-8080}
 
@@ -12,3 +6,6 @@ NUM_NODES=${NUM_NODES:-2}
 
 # Use overlay driver
 USE_OVERLAY=${USE_OVERLAY:-y}
+
+# Use non-dockerized build
+KUBEADM_DIND_LOCAL=
