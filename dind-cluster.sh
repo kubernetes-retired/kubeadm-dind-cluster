62831ae23f539b54e79af913c948e3c34751e872
Ivan Shvedunov <ishvedunov@mirantis.com>
2017-03-07 11:52:29 +0300

    Fix compatibility with docker 1.12

--- dind-cluster.sh
+++ dind-cluster.sh
@@ -656,9 +656,10 @@ function dind::down {
 # }
 
 function dind::remove-volumes {
-  docker volume ls -q -f label=mirantis.kubeadm_dind_cluster | while read volume_id; do
+  # docker 1.13+: docker volume ls -q -f label=mirantis.kubeadm_dind_cluster
+  docker volume ls -q | grep '^kubeadm-dind' | while read volume_id; do
     dind::step "Removing volume:" "${volume_id}"
-    docker volume rm -f "${volume_id}"
+    docker volume rm "${volume_id}"
   done
 }
 
