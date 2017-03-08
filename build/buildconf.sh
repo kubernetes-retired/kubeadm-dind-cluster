00b3d5cc13af2e7fc6c47f7a07f5cd987a2a01f1
Ivan Shvedunov <ishvedunov@mirantis.com>
2017-03-08 17:32:29 +0300

    Support building 1.4 & 1.5 images on Docker Cloud

--- build/buildconf.sh
+++ build/buildconf.sh
@@ -1,7 +1,17 @@
+# Version 1.4.9
+HYPERKUBE_URL_1_4_9='https://storage.googleapis.com/kubernetes-release/release/v1.4.9/bin/linux/amd64/hyperkube'
+HYPERKUBE_SHA1_1_4_9=d99b70a8496922959b6013ba2bc98f4cde868ec0
+
+# Version 1.5.3
+KUBEADM_URL_1_5_3='https://storage.googleapis.com/kubernetes-release/release/v1.5.3/bin/linux/amd64/kubeadm'
+KUBEADM_SHA1_1_5_3=e99b0aa7dd7ec0b69c4accf8b1b2039c2f2749b7
+HYPERKUBE_URL_1_5_3='https://storage.googleapis.com/kubernetes-release/release/v1.5.3/bin/linux/amd64/hyperkube'
+HYPERKUBE_SHA1_1_5_3=74dc5c4eb8c7077ecbad31d918d28a268fa447e7
+
 # url and sha1sum of kubeadm binary -- only used for prebuilt kubeadm
-KUBEADM_URL=${KUBEADM_URL:-https://storage.googleapis.com/kubernetes-release/release/v1.5.3/bin/linux/amd64/kubeadm}
-KUBEADM_SHA1=${KUBEADM_SHA1:-e99b0aa7dd7ec0b69c4accf8b1b2039c2f2749b7}
+KUBEADM_URL=${KUBEADM_URL:-${KUBEADM_URL_1_5_3}}
+KUBEADM_SHA1=${KUBEADM_SHA1:-${KUBEADM_SHA1_1_5_3}}
 
 # url and sha1sum of hyperkube binary -- only used for prebuilt hyperkube
-HYPERKUBE_URL=${HYPERKUBE_URL:-https://storage.googleapis.com/kubernetes-release/release/v1.5.3/bin/linux/amd64/hyperkube}
-HYPERKUBE_SHA1=${HYPERKUBE_SHA1:-74dc5c4eb8c7077ecbad31d918d28a268fa447e7}
+HYPERKUBE_URL=${HYPERKUBE_URL:-${HYPERKUBE_URL_1_5_3}}
+HYPERKUBE_SHA1=${HYPERKUBE_SHA1:-${HYPERKUBE_SHA1_1_5_3}}
---
+++ build/genvars.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+VERSIONS=(1.4.9 1.5.3)
+
+first=1
+for version in ${VERSIONS[@]}; do
+  base_url="https://storage.googleapis.com/kubernetes-release/release/v${version}/bin/linux/amd64"
+  if [[ ${first} ]]; then
+    first=
+  else
+    echo
+  fi
+  echo "# Version ${version}"
+  suffix="${version//./_}"
+  if [[ ! (${version} =~ ^1\.4\.) ]]; then
+      echo "KUBEADM_URL_${suffix}='${base_url}/kubeadm'"
+      echo "KUBEADM_SHA1_${suffix}=$(curl -sSL "${base_url}/kubeadm.sha1")"
+  fi
+  echo "HYPERKUBE_URL_${suffix}='${base_url}/hyperkube'"
+  echo "HYPERKUBE_SHA1_${suffix}=$(curl -sSL "${base_url}/hyperkube.sha1")"
+done
