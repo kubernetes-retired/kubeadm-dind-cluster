3293b469964ad8ee52f9c2e78820f603e4b3758b
Ivan Shvedunov <ishvedunov@mirantis.com>
2017-06-21 16:51:27 +0300

    Update to k8s 1.6.6

--- build/buildconf.sh
+++ build/buildconf.sh
@@ -8,11 +8,11 @@ KUBEADM_SHA1_1_5=f56ba79b270105cccccd9acc9c7db9895d7da81b
 HYPERKUBE_URL_1_5='https://storage.googleapis.com/kubernetes-release/release/v1.5.4/bin/linux/amd64/hyperkube'
 HYPERKUBE_SHA1_1_5=5b4209d12ae296dde2f01566ad8be3b93beff845
 
-# Version 1.6.2
-KUBEADM_URL_1_6='https://storage.googleapis.com/kubernetes-release/release/v1.6.2/bin/linux/amd64/kubeadm'
-KUBEADM_SHA1_1_6=c847244ee002f869f636eb983d10170442d19451
-HYPERKUBE_URL_1_6='https://storage.googleapis.com/kubernetes-release/release/v1.6.2/bin/linux/amd64/hyperkube'
-HYPERKUBE_SHA1_1_6=441107c3739c1b07b170d6fdc28025f21cdccacc
+# Version 1.6.6
+KUBEADM_URL_1_6='https://storage.googleapis.com/kubernetes-release/release/v1.6.6/bin/linux/amd64/kubeadm'
+KUBEADM_SHA1_1_6=e2eb3dd2b71fdfb2d5a33a260b52a8f7f3b060a7
+HYPERKUBE_URL_1_6='https://storage.googleapis.com/kubernetes-release/release/v1.6.6/bin/linux/amd64/hyperkube'
+HYPERKUBE_SHA1_1_6=bb741aba5760c751f3ed661debc583e5c8e62cd6
 
 # HYPERKUBE_URL=${HYPERKUBE_URL:-${HYPERKUBE_URL_1_4}}
 # HYPERKUBE_SHA1=${HYPERKUBE_SHA1:-${HYPERKUBE_SHA1_1_4}}
