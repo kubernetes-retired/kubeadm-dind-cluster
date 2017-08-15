4657c85120c11e35ad9120a5b9384d5d211b8620
Ivan Shvedunov <ishvedunov@mirantis.com>
2017-08-16 01:16:37 +0300

    Bump k8s versions to 1.6.8 & 1.7.3

--- build/buildconf.sh
+++ build/buildconf.sh
@@ -4,17 +4,17 @@ KUBEADM_SHA1_1_5=f56ba79b270105cccccd9acc9c7db9895d7da81b
 HYPERKUBE_URL_1_5='https://storage.googleapis.com/kubernetes-release/release/v1.5.4/bin/linux/amd64/hyperkube'
 HYPERKUBE_SHA1_1_5=5b4209d12ae296dde2f01566ad8be3b93beff845
 
-# Version 1.6.6
-KUBEADM_URL_1_6='https://storage.googleapis.com/kubernetes-release/release/v1.6.6/bin/linux/amd64/kubeadm'
-KUBEADM_SHA1_1_6=e2eb3dd2b71fdfb2d5a33a260b52a8f7f3b060a7
-HYPERKUBE_URL_1_6='https://storage.googleapis.com/kubernetes-release/release/v1.6.6/bin/linux/amd64/hyperkube'
-HYPERKUBE_SHA1_1_6=bb741aba5760c751f3ed661debc583e5c8e62cd6
+# Version 1.6.8
+KUBEADM_URL_1_6='https://storage.googleapis.com/kubernetes-release/release/v1.6.8/bin/linux/amd64/kubeadm'
+KUBEADM_SHA1_1_6=dd6ac1743103b9cfb125f32dd902ab07d2cf85a2
+HYPERKUBE_URL_1_6='https://storage.googleapis.com/kubernetes-release/release/v1.6.8/bin/linux/amd64/hyperkube'
+HYPERKUBE_SHA1_1_6=218d820797bbabe92cc036c2caa48915977c777f
 
-# Version 1.7.0
-KUBEADM_URL_1_7='https://storage.googleapis.com/kubernetes-release/release/v1.7.0/bin/linux/amd64/kubeadm'
-KUBEADM_SHA1_1_7=fca7334a26c3e14ad6fde64cd5209e0e64b3ba8c
-HYPERKUBE_URL_1_7='https://storage.googleapis.com/kubernetes-release/release/v1.7.0/bin/linux/amd64/hyperkube'
-HYPERKUBE_SHA1_1_7=50e7376ef9ebe12d8492b5ce1d70e0d9d933d814
+# Version 1.7.3
+KUBEADM_URL_1_7='https://storage.googleapis.com/kubernetes-release/release/v1.7.3/bin/linux/amd64/kubeadm'
+KUBEADM_SHA1_1_7=9a9173220c153d99565736b626d13d03cb273d2c
+HYPERKUBE_URL_1_7='https://storage.googleapis.com/kubernetes-release/release/v1.7.3/bin/linux/amd64/hyperkube'
+HYPERKUBE_SHA1_1_7=a74f4c4789b0b00c81370421dc395911e8cf54e8
 
 # KUBEADM_URL=${KUBEADM_URL:-${KUBEADM_URL_1_5}}
 # KUBEADM_SHA1=${KUBEADM_SHA1:-${KUBEADM_SHA1_1_5}}
