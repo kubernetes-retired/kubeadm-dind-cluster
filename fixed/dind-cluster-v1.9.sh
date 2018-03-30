d76bdbe6e25f58c222ecb23732535625c2dcc905
Jefferson J Hunt <jeffersonjhunt@gmail.com>
2018-03-30 08:51:46 -0500

    build/genfixed.sh to update scripts in fixed

--- fixed/dind-cluster-v1.9.sh
+++ fixed/dind-cluster-v1.9.sh
@@ -979,6 +979,7 @@ function dind::fix-mounts {
     if [[ ${BUILD_KUBEADM} || ${BUILD_HYPERKUBE} ]]; then
       docker exec "${node_name}" mount --make-shared /k8s
     fi
+    docker exec "${node_name}" mount --make-shared /sys/kernel/debug
   done
 }
 
@@ -1358,6 +1359,9 @@ case "${1:-}" in
     ;;
   clean)
     dind::clean
+    ;;
+  copy-image)
+    dind::copy-image
     ;;
   e2e)
     shift
