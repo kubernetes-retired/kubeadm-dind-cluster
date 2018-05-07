6d42bb7f86ee4613e961b25ff3dfcff83e81e239
Ivan Shvedunov <ishvedunov@mirantis.com>
2018-05-08 00:44:19 +0300

    Add fixed/dind-cluster-stable.sh
    Fixes #115.

--- build/genfixed.sh
+++ build/genfixed.sh
@@ -22,7 +22,7 @@ DIND_ROOT=$(dirname "${BASH_SOURCE}")/..
 
 fixed_dir="${DIND_ROOT}/fixed"
 mkdir -p "${fixed_dir}"
-for tag in v1.8 v1.9 v1.10; do
+for tag in v1.8 v1.9 v1.10 stable; do
   dest="${fixed_dir}/dind-cluster-${tag}.sh"
   sed "s@#%CONFIG%@EMBEDDED_CONFIG=y;DIND_IMAGE=mirantis/kubeadm-dind-cluster:${tag}@" \
       "${DIND_ROOT}/dind-cluster.sh" >"${dest}"
